a0570eb8f4a66cb9567859c5ffc248cdc12df75d
Ayush-23Y <2024.ayush.yadav@ves.ac.in>
2026-03-18 10:37:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'WebL-Exp5-69' with your repo name exactly
 export default defineConfig({
-  plugins: [react()],
+  base: '/WebL-Exp5-69/',
+  plugins: [react()]
 })
