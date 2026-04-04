62180da2c29ad7f3fb3e4de73f3674b492554fc7
Ayush-23Y <2024.ayush.yadav@ves.ac.in>
2026-04-04 20:06:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  base: '/WebL-Exp5-69/',
-  plugins: [react()]
+  plugins: [react()],
+  base: "/WebL-Exp5-69/"
 })
